b4192700b7838318fc2d90036ac2f346284d401f
Taps <tsamkelo96@gmail.com>
2024-03-12 11:57:52 -0700

    db update

--- utils/db.js
+++ utils/db.js
@@ -1,43 +1,43 @@
 import { MongoClient } from 'mongodb';
 
 class DBClient {
-    constructor () {
-        const {
-            DB_HOST = 'localhost',
-            DB_PORT = 27017,
-            DB_DATABASE = 'files_manager',
-        } = process.env;
+	constructor() {
+		const {
+			DB_HOST = 'localhost',
+			DB_PORT = 27017,
+			DB_DATABASE = 'files_manager',
+		} = process.env;
 
-        this.client = new MongoClient(`mongodb://${DB_HOST}:${DB_PORT}`,{
-            userNewUrlParser: true,
-            userUnifiedTopology: true,
-        });
+		this.client = new MongoClient(`mongodb://${DB_HOST}:${DB_PORT}`, {
+			userNewUrlParser: true,
+			userUnifiedTopology: true,
+		});
 
-        this.databaseName = DB_DATABASE;
-    }
+		this.databaseName = DB_DATABASE;
+	}
 
-    async isAlive() {
-        try {
-            await this.client.connect();
-            return true;
-        } catch (error) {
-            return false;
-        }  finally {
-            await this.client.close();
-        }
-    }
+	async isAlive() {
+		try {
+			await this.client.connect();
+			return true;
+		} catch (error) {
+			return false;
+		} finally {
+			await this.client.close();
+		}
+	}
 
-    async nbUsers() {
-        const database = this.client.db(this.databaseName);
-        const usersCollection = database.collection('users');
-        return usersCollection.countDocuments();
-    }
-    
-    async nbFiles() {
-        const database = this.client.db(this.databaseName);
-        const filesCollection = database.collection('files');
-        return filesCollection.countDocuments();
-    }
+	async nbUsers() {
+		const database = this.client.db(this.databaseName);
+		const userCollection = database.collection('users');
+		return usersCollection.countDocuments();
+	}
+
+	async nbFiles() {
+		const database = this.client.db(this.databaseName);
+		const userCollection = database.collection('files');
+		return usersCollection.countDocuments();
+	}
 }
 
 const dbClient = new DBClient();
