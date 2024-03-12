e2f90a6f7e962ccc7943222475b8c43ec2e81725
Taps <tsamkelo96@gmail.com>
2024-03-12 16:09:15 -0700

    controller

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,22 +2,34 @@ import dbClient from '../utils/db';
 
 class AppController {
     static async getStatus(req, res) {
-	const isDbAlive = await dbClient.isAlive();
+        const isDbAlive = await dbClient.isAlive();
 
-	res.status(200).json({
-            redis: true,
-            db: isDbAlive,
-	});
+        if (isDbAlive) {
+            res.status(200).json({
+                redis: true,
+                db: true,
+            });
+        } else {
+            res.status(500).json({
+                error: 'Database connection error',
+            });
+        }
     }
 
     static async getStats(req, res) {
-	const usersCount = await dbClient.nbUsers();
-	const filesCount = await dbClient.nbFiles();
+        try {
+            const usersCount = await dbClient.nbUsers();
+            const filesCount = await dbClient.nbFiles();
 
-	res.status(200).json({
-	    users: usersCount,
-	    files: filesCount,
-	});
+            res.status(200).json({
+                users: usersCount,
+                files: filesCount,
+            });
+        } catch (error) {
+            res.status(500).json({
+                error: 'Error retrieving stats',
+            });
+        }
     }
 }
 
