eab84854d317e29591a00cc24e7344e09b2cf673
Demakatso <demakatsomay@gmail.com>
2024-03-13 08:40:59 -0700

    test

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 import express from 'express';
-<<<<<<< HEAD
 import Routes from './routes/index';
 
 const app = express();
@@ -12,18 +11,3 @@ app.listen(port, () => {
 });
 
 export default app;
-=======
-import dotenv from 'dotenv';
-import routes from './routes';
-
-dotenv.config();
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.use('/', routes);
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
->>>>>>> 7e86e1ceb75b48b7a720208d50989ec23efd901f
--- worker.js
+++ worker.js
@@ -1,44 +1,13 @@
-import DBClient from './utils/db';
+import express from 'express';
+import Routes from './routes/index';
 
-const Bull = require('bull');
-const { ObjectId } = require('mongodb');
-const imageThumbnail = require('image-thumbnail');
-const fs = require('fs');
-const fileQueue = new Bull('fileQueue');
-const userQueue = new Bull('userQueue');
+const app = express();
+const port = process.env.PORT || 5000;
 
-const createImageThumbnail = async (path, options) => {
-  try {
-    const thumbnail = await imageThumbnail(path, options);
-    const pathNail = `${path}_${options.width}`;
-
-    await fs.writeFileSync(pathNail, thumbnail);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-fileQueue.process(async (job) => {
-  const { fileId } = job.data;
-  if (!fileId) throw Error('Missing fileId');
-
-  const { userId } = job.data;
-  if (!userId) throw Error('Missing userId');
-
-  const fileDocument = await DBClient.db.collection('files').findOne({ _id: ObjectId(fileId), userId: ObjectId(userId) });
-  if (!fileDocument) throw Error('File not found');
-
-  createImageThumbnail(fileDocument.localPath, { width: 500 });
-  createImageThumbnail(fileDocument.localPath, { width: 250 });
-  createImageThumbnail(fileDocument.localPath, { width: 100 });
+app.use(express.json());
+Routes(app);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
 
-userQueue.process(async (job) => {
-  const { userId } = job.data;
-  if (!userId) throw Error('Missing userId');
-
-  const userDocument = await DBClient.db.collection('users').findOne({ _id: ObjectId(userId) });
-  if (!userDocument) throw Error('User not found');
-
-  console.log(`Welcome ${userDocument.email}`);
-});
+export default app;
