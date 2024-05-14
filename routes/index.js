acb9ab050990f613693d8235a272022bc5643e8f
Taps <105137490+Thapelo-ST@users.noreply.github.com>
2024-05-14 13:49:09 +0000

    Name fix

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const AppController = require('../controllers/AppController');
-const UserController = require('../controllers/UserController');
+const UserController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
 
 // GET
